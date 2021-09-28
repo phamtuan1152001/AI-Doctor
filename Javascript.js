76465072f99ab03a1c5a924a2651cf93aa49a80b
KIddoKg <freedomv27@gmail.com>
2021-09-28 21:16:30 +0700

    change code

--- Javascript.js
+++ Javascript.js
@@ -49,16 +49,7 @@ $(document).ready(function() {
     // document ready  
     });
     $('document').ready(function(){
-        $('input[type="text"], input[type="email"], textarea').focus(function(){
-            var background = $(this).attr('id');
-            $('#' + background + '-form').addClass('formgroup-active');
-            $('#' + background + '-form').removeClass('formgroup-error');
-        });
-        $('input[type="text"], input[type="email"], textarea').blur(function(){
-            var background = $(this).attr('id');
-            $('#' + background + '-form').removeClass('formgroup-active');
-        });
-    
+        
     function errorfield(field){
         $(field).addClass('formgroup-error');
         console.log(field);	
@@ -66,18 +57,21 @@ $(document).ready(function() {
     
     $("#waterform").submit(function() {
         var stopsubmit = false;
-    
+        
+        
     if($('#name').val() == "") {
         errorfield('#name-form');
         stopsubmit=true;
     }
-    if($('#email').val() == "") {
-        errorfield('#email-form');
+    if($('#message').val() == "") {
+        errorfield('#message-form');
         stopsubmit=true;
     }
       if(stopsubmit) return false;
+      
     });
             
+
     });
     
 
