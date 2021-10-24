fffe82cdd5ad8cf5227d58f4369f3940c71ae209
Song Tuan <75135018+phamtuan1152001@users.noreply.github.com>
2021-10-24 15:30:23 +0700

    Finishing task Multi-function pharmacy and Personal/business service

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,8 +1,9 @@
 class SiteController {
   // [GET] site
   index(req, res) {
-    res.render("Person");
+    res.render("Personal_business_healthcare");
   }
 }
 
 module.exports = new SiteController();
+                                 
