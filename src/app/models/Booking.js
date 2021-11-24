e17e7294233b60f4992eeba7521b9ebe90e7c335
Do Hoang Nhung <74888302+DoHoangNhung@users.noreply.github.com>
2021-11-24 23:11:37 +0700

    Update new Booking.js model
    Due to the change in the site Booking.hbs

--- src/app/models/Booking.js
+++ src/app/models/Booking.js
@@ -14,3 +14,36 @@ const Booking = new Schema({
 })
 
 module.exports = mongoose.model('Booking', Booking)
+
+/**********************************
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+        },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    bookingid: {
+        type: String,
+        required: true
+    }  
+}, { timestamps: true });
+
+const Booking = mongoose.model('Booking', bookingSchema);
+module.exports = Booking;
+
+*****************************************/
