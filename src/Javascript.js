59ffb0bcd73400f3e2f40b29b027debaa1c1907a
Nguyễn Quốc Bảo <freedomv27@gmail.com>
2021-11-28 11:53:56 +0700

    test

--- src/Javascript.js
+++ src/Javascript.js
@@ -2,21 +2,30 @@ const express = require("express");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const path = require("path");
-const db = require("./config/db");
+const mongoose = require('mongoose');
+
 const route = require("./routes");
 const app = express();
 const port = 2000;
 const swal = require("sweetalert");
 
+
 // New
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require("passport");
+const db = require('./config/db/keys').mongoURI;
 
 app.use(express.static(path.join(__dirname, "public")));
 
 //Connect to DB
-db.connect();
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true ,useUnifiedTopology: true}
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // HTTP logger
 app.use(morgan("combined"));
@@ -68,4 +77,14 @@ route(app);
 
 app.listen(port , () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
 });
---
+++ src/config/db/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://Abc12:123456Abc12@cluster0.pfdwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
