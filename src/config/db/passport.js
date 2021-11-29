693476a626f7ff3fb497c7ba05b71aa0f6f82893
Nguyễn Quốc Bảo <freedomv27@gmail.com>
2021-11-29 08:43:55 +0700

    test

--- src/config/db/passport.js
+++ src/config/db/passport.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 // Load User model
-const User = require('../app/models/User');
+const User = require('../../app/models/User');
 
 module.exports = function(passport){
     passport.user(
