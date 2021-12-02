dc2220d4c1b0b3b44ccc1183d9e0052a1e2ffce0
Do Hoang Nhung <74888302+DoHoangNhung@users.noreply.github.com>
2021-12-02 11:16:19 +0700

    Delete booking.js

--- src/routes/booking.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const BookingController = require('../app/controllers/BookingController');
-
-const router = express.Router();
-
-// post appointment into database
-router.post('/', BookingController.booking_post);
-
-module.exports = router;
