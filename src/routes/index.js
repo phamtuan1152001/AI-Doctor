75b0b7cf8f708010d5522fc5bab5f9eaeaa2d3f6
Do Hoang Nhung <74888302+DoHoangNhung@users.noreply.github.com>
2021-12-02 11:20:50 +0700

    add index

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+
+const siteRouter = require('./site')
+const infoRouter = require('./information')
+const servicesRouter = require('./services')
+const userRouter = require('./user')
+const personRouter = require('./person')
+const bookingRouter = require('./booking')
+
+function route(app) {
+
+    app.use('/Person', personRouter)
+    app.use('/Users',userRouter);
+    app.use('/booking', bookingRouter)
+
+    app.use('/', siteRouter)
+}
+module.exports = route
+
+
