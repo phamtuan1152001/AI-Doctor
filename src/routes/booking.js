f419ff4c7087ad3c17d602788cd129df643033aa
Do Hoang Nhung <74888302+DoHoangNhung@users.noreply.github.com>
2021-12-05 10:55:51 +0700

    Update route booking.js

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -1,9 +1,75 @@
 const express = require('express');
-const BookingController = require('../app/controllers/BookingController');
-
 const router = express.Router();
 
+require('../app/models/Scheduler');
+const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+
+var scheduler = mongoose.model('Scheduler');
+var user = mongoose.model('User');
+
+const Booking = require('../app/models/Booking');
+const User = require('../app/models/User');
+const passport = require('passport')
+
 // post appointment into database
-router.post('/', BookingController.booking_post);
+router.post('/', (req, res) => {
+
+    // new booking
+    var booking = new Booking({
+        email: req.body.email,
+        bookingid : req.body.bookingid,
+        requirement : req.body.requirement
+    });
+
+    // save booking to the database
+    booking.save()
+        .then((result) => {
+            console.log(req.body);
+        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message || "Some error occur while booking!" });
+        });
+    
+    // find appointment infor to send email
+    scheduler.findOne( { bookingid : req.body.bookingid }, function(err, result) {
+        if (err) throw err;
+        var dname = result.doctor_name;
+        var department = result.department;
+        var hospital = result.hospital;
+        var date = result.date;
+        var email = result.email;
+        var phone = result.phone;
+
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            requireTLS: true,
+            auth: {
+                user: 'aidoctor.se@gmail.com',
+                pass: 'segrp32021'
+                }
+            });
+
+        var mailOptions = {
+            from: 'aidoctor.se@gmail.com',
+            to: req.body.email,
+            subject: 'Email notification of booking progress at AI Doctor',
+            html:
+            '<b>Thank you for booking an appointment at AI Doctor system!</b><p>Information for booked appointment:</p><ul><li>Doctor name: ' + dname + '</li><li>Department: ' + department + '</li><li>Hospital: ' + hospital + '</li><li>Date: ' + date + '</li><li>Email: ' + email + '</li><li>Phone: ' + phone + '</li></ul>'
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+    });
+})
 
 module.exports = router;
+
