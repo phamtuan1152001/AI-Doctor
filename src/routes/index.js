6a76a308ac10a09f8806aa781aa498540b7c0068
Do Hoang Nhung <74888302+DoHoangNhung@users.noreply.github.com>
2021-12-05 10:56:57 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const infoRouter = require('./information')
 const servicesRouter = require('./services')
 const userRouter = require('./user')
 const personRouter = require('./person')
+const bookingRouter = require('./booking')
 
 function route(app) {
 
@@ -11,7 +12,8 @@ function route(app) {
 
     app.use('/Users',userRouter);
 
-
+    app.use('/Booking', bookingRouter)
+    
     app.use('/', siteRouter)
 }
 module.exports = route
