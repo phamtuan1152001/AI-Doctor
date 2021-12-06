c243dd3f2a678d4165374e6289fa9385807f796c
Song Tuan <75135018+phamtuan1152001@users.noreply.github.com>
2021-12-06 17:12:23 +0700

    fix code tiếp tục cho Diagnose

--- src/routes/diagnose.js
+++ src/routes/diagnose.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const DiagnoseController = require('../app/controllers/DiagnoseController')
 
-router.post('/User/Services/Diagnose', DiagnoseController.diagnose)
+router.post('/diagnose', DiagnoseController.diagnose)
 router.use('/', DiagnoseController.index)
 
 module.exports = router;
